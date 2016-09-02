ca9770906669e5d807e0fcecdd41c5345823b0dd
William R. Wyatt <aidanpryde18@gmail.com>
2016-09-01 20:19:12 -0400

    Add Code Challenge Work

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,15 @@
 //create function
-  //control and return statements
+function checkInput(input) {
+//control and return statements
+	return (input == 'Hello');
+}
 
 //Prompt user for input and store variable
-
+var userInput = prompt("Say 'Hello' to me!");
 //Call function
-
+while ( !checkInput(userInput) ) {
+	alert('Sorry, that is the wrong word :(');
+	userInput = prompt("Say 'Hello' to me!");
+}
 //Alert user results
+alert('Hello, World!');
