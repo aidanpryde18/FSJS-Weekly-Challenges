cf16372e56bc3b5a75b33fff1dad9049aecd8281
William R. Wyatt <aidanpryde18@gmail.com>
2016-10-05 16:58:25 -0400

    Add Challenge 6

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -1,4 +1,5 @@
 function openBlock (id) {
+  console.log(id);
     var block = document.getElementById(id);
     var blocks = document.getElementById("main_content").getElementsByTagName("div");
 
@@ -6,14 +7,22 @@ function openBlock (id) {
         blocks[y].style.display = "none";
     }
     block.style.display = "block";
-    
+
     // TODO: remove the "active" class from all of the li elements inside the menu
+    $("#menu").children().removeClass("active");
     // TODO: add the "active" class to the li element that contains the link that was clicked
 
 }
+$("#menu").children().click(function() {
+  openBlock($(this).children().attr("id"))
+  $(this).addClass("active");
+});
 
 // TODO: add the "hover" class to the menu items when you hover over them
-
+$("#menu").children().children().mouseenter(function() {
+  $(this).toggleClass("hover")
+}).mouseleave(function() {
+  $(this).toggleClass("hover")
+});
 
 // TODO: set up the tooltip plugin on all of the links in the menu
-
--- Challenges/Week6/js/app.js
+++ Challenges/Week6/js/app.js
@@ -1,11 +1,45 @@
 //#Week 6 JQuery Code Challenge
 
+function updateOutput() {
+    var html = "<br>Box Height: " + Box.height + "<br>" + "Box Width: " + Box.width + "<br>" + "Box Volume: " + Box.volume();
+    $('#output').html(html);
+}
 //Objects
 		
 //Code Challenge:
 //Create an object named "Box" with 3 properties, height, width, volume.
+var Box = {
+    height: 3,
+    width: 3,
+    volume: function () {
+        var volume = this.height * this.width;
+        return volume;
+    }
+}
 //Create 2 buttons for Height. The first button decreases the Box Height by 1. The second button increases the Box Height by 1.
+$('#HeightDecrease').click(function () {
+    if(Box.height > 0) {
+        Box.height--;
+        updateOutput();
+    }
+});
+$('#HeightIncrease').click(function () {
+    Box.height++;
+    updateOutput();
+});
+$('#WidthDecrease').click(function () {
+    if(Box.width > 0) {
+        Box.width--;
+        updateOutput();
+    }
+});
+$('#WidthIncrease').click(function () {
+    Box.width++;
+    updateOutput();
+});
 //Create a button that prints the object and its attributes to the page (use the span "output".
- 
+$('#Print').click(function () {
+    updateOutput()
+});
 //Extra credit
 //Create interactive buttons to decrease or increase the Width and Volume
